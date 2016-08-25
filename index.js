501a98985818f824edb2b550d3ef145d8b1ee1a4
durden2 <gandor.michal@gmail.com>
2016-08-25 20:39:27 +0200

    Socket io

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+require("babel-polyfill");
+
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(4000, function(){
